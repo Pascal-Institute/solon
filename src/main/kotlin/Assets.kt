94e6c118753a40eb1ded47ea69fbf62b8c82b893
volta2030 <volta2030@gmail.com>
2024-06-29 17:31:31 +0900

    feat : implementation currentValue & nonCurrentValue of assets, liabilities

--- src/main/kotlin/Assets.kt
+++ src/main/kotlin/Assets.kt
@@ -1,5 +1,17 @@
 //Assets : 개인이나 기업이 보유하고 있는 경제적 가치
 data class Assets(val equity: Equity, val liabilities: Liabilities){
+
+    //유동 자산 : 1년이내에 돈이 되는 자산
+    var currentValue = getTotal()
+
+    //비유동 자산 : 돈이 되는데 1년 이상 걸리는 자산
+    var nonCurrentValue = getTotal() - currentValue
+
+    fun setCurrentValue(value : Double){
+        currentValue = value
+        nonCurrentValue = getTotal() - currentValue
+    }
+
     fun getTotal() : Double{
         return equity.getTotal() + liabilities.getTotal()
     }
--- src/main/kotlin/Liabilities.kt
+++ src/main/kotlin/Liabilities.kt
@@ -1,6 +1,10 @@
 //Liabilities : 자산을 얻기 위해 회사가 외부에서 차입한 자금
-data class Liabilities(var value : Double){
+data class Liabilities(var currentValue : Double, var nonCurrentValue : Double){
+
+    //currentValue(유동 부채) : 1년 안에 갚아야 되는 부채
+    //nonCurrentValue(비유동 부채) : 만기가 1년 이상 남아 있는 부채
+
     fun getTotal() : Double{
-        return value
+        return currentValue + nonCurrentValue
     }
 }
