708cdfa08be5d4ec6e7ce684e54a1c578852695a
volta2030 <volta2030@gmail.com>
2024-06-29 17:32:21 +0900

    fix : change Company

--- src/main/kotlin/Company.kt
+++ src/main/kotlin/Company.kt
@@ -1,6 +1,8 @@
+//회사가 돈을 버는 방법, 1.장사 2.증자
+
 class Company {
     //Default is A Million Dollars
-    var assets = Assets(Equity(1.0), Liabilities(2.0))
+    var assets = Assets(Equity(1.0), Liabilities(2.0, 3.0))
 
     fun isCapitalErosion() : Boolean{
         return assets.getTotal() < assets.liabilities.getTotal()
