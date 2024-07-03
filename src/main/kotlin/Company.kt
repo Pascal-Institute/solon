1262556540ebceca3353e8a03ffce10ce0f9a130
volta2030 <volta2030@gmail.com>
2024-07-03 22:37:03 +0900

    feat : add stockPrice

--- src/main/kotlin/Company.kt
+++ src/main/kotlin/Company.kt
@@ -4,6 +4,9 @@ class Company {
     //Default is A Million Dollars
     var assets = Assets(Equity(1.0), Liabilities(2.0, 3.0))
 
+    //Default is A Dollar
+    var stockPrice = 1.0
+
     fun isCapitalErosion() : Boolean{
         return assets.equity.getTotal() < assets.liabilities.getTotal()
     }
